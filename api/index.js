12ca9858dd9d5caf6df8997afafe41c3c83193c2
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-03-31 09:21:16 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,18 +1,11 @@
 const express = require("express");
 const app = express();
-const expressApp = require("../public/express/express.js" );
-
-app.use(expressApp);
-
-app.use(express.static('public'));
 
 app.get("/express", (req, res) => {
-    res.sendFile('../public/express/lista.html', {root: '.'});
+    res.sendFile("./public/express/lista.html", { root: '.' });
 });
-
 app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
-
-app.get("/datos", (req, res) => res.json(datos));
+app.use(express.static('public'))
 
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
