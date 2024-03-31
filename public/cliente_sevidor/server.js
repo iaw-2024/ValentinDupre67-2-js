ffb9e90528c233e8a188b18b42bc912ed0340d78
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-03-31 20:29:16 -0300

    Update server.js

--- public/cliente_sevidor/server.js
+++ public/cliente_sevidor/server.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const tablaMotos = document.getElementById('cuerpoDeMotos');
 
-    fetch('http://localhost:3001/datos.js')
+    fetch('http://localhost:3001/datos.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Error en la solicitud de datos');
