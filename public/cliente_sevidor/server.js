6d192076c20ef057a6865db2b13da33c39dc062f
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-03-31 19:08:09 -0300

    .

--- public/cliente_sevidor/server.js
+++ public/cliente_sevidor/server.js
@@ -1,20 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const tablaMotos = document.getElementById('cuerpoDeMotos');
-    fetch('http://localhost:3001/datos') 
-        .then(response => response.json())
-        .then(data => {            
-            data.forEach(moto => {
-                const fila = document.createElement('tr');
-                fila.innerHTML = `
-                    <td>${moto.Marca}</td>
-                    <td>${moto.Modelo}</td>
-                    <td>${moto.Cilindrada}</td>
-                    <td>${moto.VelocidadMaxima}</td>
-                    <td>${moto.Color}</td>
-                    <td>${moto.CapacidadTanque}</td>
-                `;
-                tablaMotos.appendChild(fila);
-            });
+window.onload = function() {
+    let tablaDeMotos = document.getElementById('tablaDeMotos');
+
+    fetch('http://localhost:3001/datos')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la solicitud de datos');
+            }
+            return response.json();
+        })
+        .then(datos => {
+            for (let moto of datos) {
+                let fila = document.createElement('tr');
+
+                for (let propiedad in moto) {
+                    let celda = document.createElement('td');
+                    celda.textContent = moto[propiedad];
+                    fila.appendChild(celda);
+                }
+
+                tablaDeMotos.appendChild(fila);
+            }
         })
         .catch(error => console.error('Error al obtener datos:', error));
-});
+}
+
--- public/dom/dom.js
+++ public/dom/dom.js
@@ -1,26 +1,15 @@
 window.onload = function() {
   let tablaDeMotos = document.getElementById('tablaDeMotos');
 
-  fetch('http://localhost:3001/datos')
-      .then(response => {
-          if (!response.ok) {
-              throw new Error('Error en la solicitud de datos');
-          }
-          return response.json();
-      })
-      .then(datos => {
-          for (let moto of datos) {
-              let fila = document.createElement('tr');
+  for (let moto of datos) {
+    let fila = document.createElement('tr'); 
 
-              for (let propiedad in moto) {
-                  let celda = document.createElement('td');
-                  celda.textContent = moto[propiedad];
-                  fila.appendChild(celda);
-              }
+    for (let propiedad in moto) {
+      let celda = document.createElement('td');
+      celda.textContent = moto[propiedad];
+      fila.appendChild(celda);
+    }
 
-              tablaDeMotos.appendChild(fila);
-          }
-      })
-      .catch(error => console.error('Error al obtener datos:', error));
-}
-
+    tablaDeMotos.appendChild(fila);
+  }
+};
