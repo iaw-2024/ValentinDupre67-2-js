0e69c4c14e03af7c5b9218e1df1ed68cff4593e2
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-03-31 10:03:37 -0300

    Update express.js

--- public/express/express.js
+++ public/express/express.js
@@ -3,7 +3,28 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 
-const datos = require('../../public/datos.js');
+const datos = [
+    { marca: 'Yamaha', modelo: 'YZF-R1', cilindrada: 998, velocidadMaxima: 285, color: 'Azul', capacidadTanque: 17 },
+    { marca: 'Honda', modelo: 'CBR1000RR', cilindrada: 1000, velocidadMaxima: 299, color: 'Rojo', capacidadTanque: 16 },
+    { marca: 'Honda', modelo: 'CBR600RR', cilindrada: 600, velocidadMaxima: 252, color: 'Blanco', capacidadTanque: 18 },
+    { marca: 'Honda', modelo: 'CBR500R', cilindrada: 500, velocidadMaxima: 185, color: 'Negro', capacidadTanque: 17.1 },
+    { marca: 'Honda', modelo: 'CBR300R', cilindrada: 300, velocidadMaxima: 157, color: 'Azul', capacidadTanque: 13 },
+    { marca: 'Honda', modelo: 'CBR125R', cilindrada: 125, velocidadMaxima: 120, color: 'Rojo', capacidadTanque: 13 },
+    { marca: 'Ducati', modelo: 'Panigale V4', cilindrada: 1103, velocidadMaxima: 290, color: 'Rojo', capacidadTanque: 16 },
+    { marca: 'BMW', modelo: 'S1000RR', cilindrada: 999, velocidadMaxima: 299, color: 'Blanco', capacidadTanque: 17.5 },
+    { marca: 'Kawasaki', modelo: 'Ninja ZX-10R', cilindrada: 998, velocidadMaxima: 299, color: 'Verde', capacidadTanque: 17 },
+    { marca: 'Suzuki', modelo: 'GSX-R1000', cilindrada: 999, velocidadMaxima: 299, color: 'Azul', capacidadTanque: 16 },
+    { marca: 'Aprilia', modelo: 'RSV4 1100', cilindrada: 1078, velocidadMaxima: 290, color: 'Negro', capacidadTanque: 18.5 },
+    { marca: 'KTM', modelo: 'RC16', cilindrada: 1000, velocidadMaxima: 310, color: 'Naranja', capacidadTanque: 20 },
+    { marca: 'Triumph', modelo: 'Daytona 675', cilindrada: 675, velocidadMaxima: 260, color: 'Negro', capacidadTanque: 17.4 },
+    { marca: 'Yamaha', modelo: 'YZF-R6', cilindrada: 599, velocidadMaxima: 257, color: 'Blanco', capacidadTanque: 17 },
+    { marca: 'Yamaha', modelo: 'YZF-R3', cilindrada: 321, velocidadMaxima: 188, color: 'Negro', capacidadTanque: 14 },
+    { marca: 'Yamaha', modelo: 'YZF-R125', cilindrada: 125, velocidadMaxima: 120, color: 'Rojo', capacidadTanque: 11.5 },
+    { marca: 'Yamaha', modelo: 'MT-10', cilindrada: 998, velocidadMaxima: 250, color: 'Gris', capacidadTanque: 17 },
+    { marca: 'Harley Davidson', modelo: 'Street 750', cilindrada: 749, velocidadMaxima: 180, color: 'Negro', capacidadTanque: 13.1 },
+    { marca: 'Indian', modelo: 'Scout Bobber', cilindrada: 1133, velocidadMaxima: 180, color: 'Rojo', capacidadTanque: 12.5 },
+    { marca: 'Royal Enfield', modelo: 'Interceptor 650', cilindrada: 648, velocidadMaxima: 170, color: 'Plata', capacidadTanque: 13.7 }
+  ];
 
 function generarTabla(datos) {
     let tablaHTML = '<table id="ReemplazarPorTabla">';
