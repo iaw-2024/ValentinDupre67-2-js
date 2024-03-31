c9b4ee8c96f7cce40d7dfcf84c872aa665074d96
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-03-31 11:22:46 -0300

    se modifico el api

--- api/index.js
+++ api/index.js
@@ -13,9 +13,7 @@ app.get("/express", (req, res) => {
 
 app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
 
-app.get("/datos", (req, res) => {
-    res.sendFile("../public/datos.js");
-});
+app.get("/datos", (req, res) => res.json(datos));
 
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
