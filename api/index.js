b1ebdb263f96eebcd74ac997ef1460092277ec72
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-03-31 20:35:12 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,11 +11,13 @@ app.get("/express", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/express/lista.html'));
 });
 
-app.get("/cliente_servidor", (req, res) => res.sendFile(path.join(__dirname, '../public/cliente_sevidor/index.html')))
+app.get("/cliente_servidor", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/cliente_sevidor/index.html'));
+});
 
-/* TODO preguntar si es necesartio 
-app.get("/datos", (req, res) => res.json(datos));
-*/
+app.get("/datos2", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/datos2.json'));
+});
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
 
