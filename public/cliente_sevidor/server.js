3dfa12f91d30f934b28a498bb63e289614cf6348
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-03-31 20:25:38 -0300

    Update server.js

--- public/cliente_sevidor/server.js
+++ public/cliente_sevidor/server.js
@@ -1,26 +1,27 @@
-window.onload = function() {
-    const tablaDeMotos = document.getElementById('cuerpoDeMotos');
-    console.log(fetch('http://localhost:3001/datos'));
-    fetch('http://localhost:3001/datos')
+
+document.addEventListener('DOMContentLoaded', () => {
+    const tablaMotos = document.getElementById('cuerpoDeMotos');
+
+    fetch('http://localhost:3001/datos.js')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Error en la solicitud de datos');
             }
             return response.json();
         })
-        .then(datos => {
+        .then(datos => {            
             for (let moto of datos) {
                 const fila = document.createElement('tr');
                 fila.innerHTML = `
-                    <td>${moto.Marca}</td>
-                    <td>${moto.Modelo}</td>
-                    <td>${moto.Cilindrada}</td>
-                    <td>${moto.VelocidadMaxima}</td>
-                    <td>${moto.Color}</td>
-                    <td>${moto.CapacidadTanque}</td>
+                    <td>${moto.marca}</td>
+                    <td>${moto.modelo}</td>
+                    <td>${moto.cilindrada}</td>
+                    <td>${moto.velocidadMaxima}</td>
+                    <td>${moto.color}</td>
+                    <td>${moto.capacidadTanque}</td>
                 `;
-                tablaDeMotos.appendChild(fila);
+                tablaMotos.appendChild(fila);
             }
         })
         .catch(error => console.error('Error al obtener datos:', error));
-};
+});
