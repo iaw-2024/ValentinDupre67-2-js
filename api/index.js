efd01eb3472c4a6eda0ff5dfe0b9720a3aa1397a
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-03-31 09:24:54 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ app.get("/express", (req, res) => {
 app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
 app.use(express.static('public'))
 
+app.use(express.static('public'))
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
 
