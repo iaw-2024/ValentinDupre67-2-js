8fbc55c97ec9fc099abbdd98f4a0fdb02feb2293
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-03-31 16:42:45 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const expressApp = require("../public/express/express.js" );
-const datos = require('../public/datos.js');
 
 app.use(expressApp);
 
