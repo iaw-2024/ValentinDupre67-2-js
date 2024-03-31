04e81fcc0a3389491a5daeeac024ce9e21393e63
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-03-31 16:48:23 -0300

    cambio de lista a index

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ app.use(expressApp);
 app.use(express.static('public'));
 
 app.get("/express", (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/express/lista.html'));
+    res.sendFile(path.join(__dirname, '../public/express/index.html'));
 });
 
 app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
