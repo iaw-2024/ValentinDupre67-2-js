f616420cced1e126740d861905c2014c598f401d
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-04-01 11:32:15 -0300

    Update server.js

--- public/cliente_sevidor/server.js
+++ public/cliente_sevidor/server.js
@@ -1,7 +1,22 @@
-fetch('https://66040fca2393662c31d081d9.mockapi.io/Motos')
-  .then(response => response.json())
-  .then(data => {
-    // Aquí puedes trabajar con los datos obtenidos, por ejemplo:
-    console.log(data);
-  })
-  .catch(error => console.error('Error:', error));
+window.onload = function() {
+    let tablaDeMotos = document.getElementById('tablaDeMotos');
+    URL = "https://66040fca2393662c31d081d9.mockapi.io/Motos";
+    console.log(URL);
+    fetch(URL) 
+        console.log(URL)      
+        .then(response => response.json())
+        .then(data => {
+            for (let moto of data) {
+            let fila = document.createElement('tr'); 
+    
+            for (let propiedad in moto) {
+                let celda = document.createElement('td');
+                celda.textContent = moto[propiedad];
+                fila.appendChild(celda);
+            }
+    
+            tablaDeMotos.appendChild(fila);
+            }
+        })
+        .catch(error => console.error('Error al obtener los datos:', error));
+    };
