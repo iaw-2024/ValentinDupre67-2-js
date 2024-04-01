5555ca1e640219d71663029b0ed0d01fff7c6b80
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-04-01 11:11:10 -0300

    Update server.js

--- public/cliente_sevidor/server.js
+++ public/cliente_sevidor/server.js
@@ -1,21 +1,4 @@
-window.onload = function() {
-    let tablaDeMotos = document.getElementById('tablaDeMotos');
-    URL = "http://localhost:3001/datos.json";
-    console.log(URL);
-    fetch(URL)        
-      .then(response => response.json())
-      .then(data => {
-        for (let moto of data) {
-          let fila = document.createElement('tr'); 
-  
-          for (let propiedad in moto) {
-            let celda = document.createElement('td');
-            celda.textContent = moto[propiedad];
-            fila.appendChild(celda);
-          }
-  
-          tablaDeMotos.appendChild(fila);
-        }
-      })
-      .catch(error => console.error('Error al obtener los datos:', error));
-  };
+$.getJSON('http://localhost:3001/datos.json', function(data) {
+  // Aquí puedes trabajar con los datos obtenidos, por ejemplo:
+  console.log(data);
+});
