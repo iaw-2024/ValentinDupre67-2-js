ad4436c242c935e5bcbf8a761b8fa85f74085453
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-04-01 09:54:50 -0300

    Update dom.js

--- public/dom/dom.js
+++ public/dom/dom.js
@@ -1,20 +1,21 @@
 window.onload = function() {
-  const tablaDeMotos = document.getElementById('cuerpoDeMotos');
+  let tablaDeMotos = document.getElementById('tablaDeMotos');
 
-  fetch('../datos.json') 
-      .then(response => {
-          if (!response.ok) {
-              throw new Error('Error en la solicitud de datos');
-          }
-          return response.json();
-      })
-      .then(datos => {
-          for (let moto of datos) {
-              const fila = document.createElement('tr');
-              fila.innerHTML = '<td>${moto.Marca}</td><td>${moto.Modelo}</td><td>${moto.Cilindrada}</td><td>${moto.VelocidadMaxima}</td><td>${moto.Color}</td><td>${moto.CapacidadTanque}</td>';
-              tablaDeMotos.appendChild(fila);
-          }
-      })
-      .catch(error => console.error('Error al obtener datos:', error));
+  fetch('../datos.json')
+    .then(response => response.json())
+    .then(data => {
+      for (let moto of data) {
+        let fila = document.createElement('tr'); 
+
+        for (let propiedad in moto) {
+          let celda = document.createElement('td');
+          celda.textContent = moto[propiedad];
+          fila.appendChild(celda);
+        }
+
+        tablaDeMotos.appendChild(fila);
+      }
+    })
+    .catch(error => console.error('Error al obtener los datos:', error));
 };
 
